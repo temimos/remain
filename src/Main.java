18f7ca8921e3d9226596427a088efd9c784b2787
temi <temitopem@gmail.com>
2019-06-20 10:31:32 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 3;
+
+        System.out.println("i is " + i);
+        System.out.println("j is " + j);
+
+        int k = i % j;
+        System.out.println("i%j is " + k);
+    }
+}
